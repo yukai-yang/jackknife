7d80f969ae04b525b3ac4f26a858426e2dd05d1c
Yukai Yang <yukai.yang@gmail.com>
2019-09-27 14:05:28 +0200

    make the descriptions

--- R/jackknife.R
+++ R/jackknife.R
@@ -1,8 +1,56 @@
-# tfunc, the statistic function T which is a function of rho
-# dfunc, delta function, input a m tuple and return a scalar
-# im the window size or tuple size
-# vw the weights for deleting or downweighting blocks with length l
-# ... other arguments for tfunc and dfunc
+#################################################################################
+## package name: jackknife
+## author: Yukai Yang
+## Statistiska Inst., Uppsala Universitet
+#################################################################################
+
+
+#' Implementing the jackknife method of estimating standard errors
+#'
+#' Implementing the jackknife method of estimating standard errors of some linear statistics
+#'
+#' The function takes several arguments including functions and returns a list containing the results.
+#'
+#' @param vx a vector of observations
+#' @param tfunc the function which takes the rho argument and returns the statistic
+#' @param dfunc the delta function which takes a tuple or window of the observations and returns a vector of deltas
+#' @param im the tuple or window size
+#' @param vw the weights for deleting or downweighting the blocks
+#' @param ... other arguments for both \code{tfunc} and \code{dfunc} if any
+#'
+#' @return a list containing the results
+#'
+#' The object is a list containing the following components:
+#' \item{T}{the value of the statistic}
+#' \item{jack}{a vector or matrix containing the jackknifed statistics}
+#' \item{sig2}{the jackknifed estimate of the variance of the statistic}
+#'
+#' @author Yukai Yang, \email{yukai.yang@@statistik.uu.se}
+#'
+#' @keywords jackknife
+#'
+#' @examples
+#' vw1 = 1
+#' vw2 = c(0.25,0.75,1,0.75,0.25)
+#'
+#' # the AR(1) model
+#' iN = 100
+#' vx <- rnorm(iN)
+#'
+#' # for first order autocorrelation
+#' # dfunc vec 2, y_t and y_t-1
+#' dfunc <- function(yy, my, vy) return(prod(yy - my)/vy)
+#'
+#' tfunc <- function(rho, my, vy) return(rho)
+#'
+#' # results
+#' ret = jackknife(as.vector(vx), tfunc, dfunc, im=2, vw=vw1, my=mean(vx), vy=var(vx))
+#' ret
+#'
+#' ret = jackknife(as.vector(vx), tfunc, dfunc, im=2, vw=vw2, my=mean(vx), vy=var(vx))
+#' ret
+#'
+#' @export
 jackknife <- function(vx, tfunc, dfunc, im, vw, ...){
   iN = length(vx)
   nn = iN - im + 1
